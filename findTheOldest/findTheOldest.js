71ba6420c1ceb9ef9b6d3be3c9143cf7d216f5d0
EJM <erin.jay.moyer@gmail.com>
2021-03-14 13:32:33 -0600

    Cleaned up findTheOldest code. Sectioned off old code for reference.

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,21 +1,22 @@
 let findTheOldest = function(array) {
-let agesArray = array; 
-
-
-agesArray.forEach (person => {
-    person['age'] = 0;
-    if (person.yearOfDeath > 0) //dead
-    { 
+    array.forEach (person => {
+        person['age'] = 0;
+        if (person.yearOfDeath > 0) //if person is dead
+        { 
         person['age'] = person.yearOfDeath - person.yearOfBirth;
-    }
-    else //alive
-    {
-        let date = new Date().getFullYear();
-        person['age'] = date - person.yearOfBirth;
-    }
-});
+        }
+        else //person is alive
+        {
+            let date = new Date().getFullYear();
+            person['age'] = date - person.yearOfBirth;
+        }
+    });
 
-/* MY OG CODE ..> 
+    array.sort((person1, person2) => (person1.age < person2.age ) ? 1 : -1);   // sort by age
+    return array[0];
+
+/* MY Original CODE 
+let agesArray = array; 
     for (arrayPlace = 0; arrayPlace < array.length; arrayPlace++ )
         if (array[arrayPlace]['yearOfDeath']) //dead
         {
@@ -28,11 +29,11 @@ agesArray.forEach (person => {
             agesArray[arrayPlace]['age'] = date - array[arrayPlace]['yearOfBirth']; 
             console.log(agesArray);
         }
+
+agesArray.sort((person1, person2) => (person1.age < person2.age ) ? 1 : -1);   // sort by age
+return agesArray[0];
 */
 
-    // sort by age
-    agesArray.sort((person1, person2) => (person1.age < person2.age ) ? 1 : -1);
-    return agesArray[0];
 
 
 }
